63f79aad253d469fe78f7c4b52c4709f5e087865
darsnisatheesh <darsudarsu19@gmail.com>
2026-07-20 17:41:08 +0530

    added pics

--- product.js
+++ product.js
@@ -35,7 +35,37 @@ const products = {
         price: "₹749",
         image: "images/Royal blue v-neck.jpeg",
         sizes:["M", "L", "XL"]
-    }
+    },
+
+     7: {
+        name: "Black Bliss Everyday Top",
+        price: "₹359",
+        image: "images/black top.jpeg",
+        sizes:["M", "L", "XL"]
+    },
+
+     8: {
+        name: "Blackpink Geometry-shaped Everyday Top ",
+        price: "₹359",
+        image: "images/blackpink.jpeg",
+        sizes:["M", "L", "XL"]
+    },
+
+     9: {
+        name: "Maroon and White Everyday Top",
+        price: "₹359",
+        image: "images/maroon.jpeg",
+        sizes:["M", "L", "XL"]
+    },
+
+     10: {
+        name: "Red Bloom Everyday Top",
+        price: "₹359",
+        image: "images/red blue.jpeg",
+        sizes:["M", "L", "XL"]
+    },
+
+
 };
 
 const params = new URLSearchParams(window.location.search);
@@ -46,7 +76,7 @@ if (products[id]) {
     document.getElementById("product-name").textContent = products[id].name;
     document.getElementById("product-price").textContent = products[id].price;
 
-    const phone = "916383351495"; 
+    const phone = "916381288411";
 
     document.getElementById("whatsapp-btn").href =
         `https://wa.me/${phone}?text=Hi! I want to order ${products[id].name} (${products[id].price})`;
